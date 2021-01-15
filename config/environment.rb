82b109048a7befe3a78a753c9c2bd7e008883722
Tom <trfreecodecamp@gmail.com>
2021-01-15 17:18:51 -0500

    Formatting

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Get rid of the "fields_with_error" tag that Rails adds to an error.
+#Use Bootstrap instead
+ActionView::Base.field_error_proc = Proc.new do|html_tag, instance|
+  html_tag.html_safe
+end
